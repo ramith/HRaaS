6d950397a834ceed9ae971e22e5af138470a5bbc
Ramith Jayasinghe <ramithj@live.com>
2022-08-17 21:09:42 +0530

    intial commit of code

---
+++ src/test/java/com/ramith/hraas/HraasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramith.hraas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HraasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
